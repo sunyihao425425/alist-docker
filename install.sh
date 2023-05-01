d1a74681fd22d7c516823b1be2ee2de5bf556737
sunyihao425425 <84254845+sunyihao425425@users.noreply.github.com>
2023-05-01 08:34:11 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 rm alist-linux-musl-amd64.tar.gz
-wget https://github.com/alist-org/alist/releases/download/v3.16.3/alist-linux-amd64.tar.gz
-tar -zxvf alist-linux-amd64.tar.gz
-rm alist-linux-amd64.tar.gz
+wget https://github.com/alist-org/alist/releases/download/v3.16.1/alist-linux-musl-amd64.tar.gz
+tar -zxvf alist-linux-musl-amd64.tar.gz
+rm alist-linux-musl-amd64.tar.gz
 chmod +x alist
 
 echo FROM scratch > Dockerfile
